c92622d435a2f5f88d64a5e42e7afe0c819a7d7f
dodor <doris.choi92@gmail.com>
2023-02-09 13:45:40 +0900

    Initial Commit

---
+++ SampleApps/SampleAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppsApp.swift
+//  SampleApps
+//
+//  Created by Do-Young Choi on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
